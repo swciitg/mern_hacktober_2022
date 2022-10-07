8e112b929f615a87cad951060ab3f148af0c29ef
Subash Paul <subashpaul559@gmail.com>
2022-10-07 10:57:09 +0530

    Added header component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,10 @@
+import Header from "./components/Header/Header";
 
 
 function App() {
   return (
     <div>
-      Happy Hactober Fest!!
+      <Header />
     </div>
   );
 }
---
+++ frontend/src/components/Header/Header.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import "./header.css"
+const Header = () => {
+    return (
+        <div>
+            <div className="header">
+                <div className="left">
+                    <span>Hi, Dongre Arindam Rajiv</span>
+                </div>
+                <div className="right">
+                    <span>Signout</span>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Header

e3c35edd59efd5864f61211ca189f4167043ce19
KaptainCS3 <mbiapplegryn@gmail.com>
2022-10-07 01:14:42 +0100

    Created Footer component

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Footer = () =>{
+    return (
+        <footer className='footer'>
+            <p>Built by Students’ Web Committee</p>
+        </footer>
+    )
+}

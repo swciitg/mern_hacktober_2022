9f7a38c9c156cd7f15cb88256a2aff6b77d3eb8c
Subash Paul <subashpaul559@gmail.com>
2022-10-07 21:54:36 +0530

    Added sibedar and header

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,9 @@
-import Header from "./components/Header/Header";
-
+import Home from "./components/Home/Home";
 
 function App() {
   return (
     <div>
-      <Header />
+      <Home />
     </div>
   );
 }
